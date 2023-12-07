c64170a2a414ee1fbdcf2242b7c954a0819f000e
Rune Nielsen <runenielsen@runbox.com>
2023-12-07 14:05:42 +0100

    now supports zero downtime deployment (#28)

--- src/RouteNetworkSearchIndexer/RouteNetworkSearchIndexerHost.cs
+++ src/RouteNetworkSearchIndexer/RouteNetworkSearchIndexerHost.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using OpenFTTH.EventSourcing;
 using RouteNetworkSearchIndexer.Config;
 using System.Collections.Generic;
+using System;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -32,9 +33,6 @@ internal sealed class RouteNetworkSearchIndexerHost : BackgroundService
     {
         _logger.LogInformation($"Starting {nameof(RouteNetworkSearchIndexerHost)}.");
 
-        // We migrate when first time run.
-        await MigratePreviousCollection().ConfigureAwait(false);
-
         _logger.LogInformation(
             "Creating Typesense collection '{CollectionName}'.",
             TypesenseCollectionConfig.CollectionName);
@@ -44,29 +42,48 @@ internal sealed class RouteNetworkSearchIndexerHost : BackgroundService
         await _eventStore.DehydrateProjectionsAsync(stoppingToken).ConfigureAwait(false);
         _logger.LogInformation("Finished initial dehydration.");
 
-        _logger.LogInformation(
-            "Upserting {CollectionAlias} to {CollectionName}.",
-            TypesenseCollectionConfig.AliasName,
-            TypesenseCollectionConfig.CollectionName);
-
-        await _typesense
-            .UpsertCollectionAlias(
-                TypesenseCollectionConfig.AliasName,
-                new CollectionAlias(TypesenseCollectionConfig.CollectionName))
-            .ConfigureAwait(false);
-
-        var collections =
-            (await RetrieveMatchingCollectionNames(
-                $"{TypesenseCollectionConfig.AliasName}-")
-             .ConfigureAwait(false))
-            .Where(x => x != TypesenseCollectionConfig.CollectionName)
-            .ToList()
-            .AsReadOnly();
-
-        foreach (var collectionName in collections)
+        try
         {
-            _logger.LogInformation("Deleting {Collection}.", collectionName);
-            await _typesense.DeleteCollection(collectionName).ConfigureAwait(false);
+            _logger.LogInformation(
+                "Upserting {CollectionAlias} to {CollectionName}.",
+                TypesenseCollectionConfig.AliasName,
+                TypesenseCollectionConfig.CollectionName);
+
+            await _typesense
+                .UpsertCollectionAlias(
+                    TypesenseCollectionConfig.AliasName,
+                    new CollectionAlias(TypesenseCollectionConfig.CollectionName))
+                .ConfigureAwait(false);
+
+            var collections =
+                (await RetrieveMatchingCollectionNames(
+                    $"{TypesenseCollectionConfig.AliasName}-")
+                 .ConfigureAwait(false))
+                .Where(x => x != TypesenseCollectionConfig.CollectionName)
+                .ToList()
+                .AsReadOnly();
+
+            foreach (var collectionName in collections)
+            {
+                _logger.LogInformation("Deleting {Collection}.", collectionName);
+                await _typesense.DeleteCollection(collectionName).ConfigureAwait(false);
+            }
+
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(
+                "Got an {Exception}, cleaning up, deleting {Collection}.",
+                ex,
+                TypesenseCollectionConfig.CollectionName);
+
+            // In case an error is thrown before we are ready, we want to cleanup
+            // the newly created collection to avoid a lot of zombie collections.
+            await _typesense
+                .DeleteCollection(TypesenseCollectionConfig.CollectionName)
+                .ConfigureAwait(false);
+
+            throw;
         }
 
         File.Create("/tmp/healthy");
@@ -89,28 +106,13 @@ internal sealed class RouteNetworkSearchIndexerHost : BackgroundService
         return Task.CompletedTask;
     }
 
-    private async Task MigratePreviousCollection()
-    {
-        try
-        {
-            await _typesense.DeleteCollection(TypesenseCollectionConfig.AliasName)
-                .ConfigureAwait(false);
-
-            _logger.LogInformation("Migration: deleting previous collection.");
-        }
-        catch (TypesenseApiNotFoundException)
-        {
-            // this is valid in case we have already migrated.
-        }
-    }
-
     private async Task SetupCollection()
     {
         var schema = new Schema(
             name: TypesenseCollectionConfig.CollectionName,
             fields: new List<Field>
             {
-                    new Field("name", FieldType.String, false, true),
+                new Field("name", FieldType.String, false, true),
             });
 
         await _typesense.CreateCollection(schema).ConfigureAwait(false);
